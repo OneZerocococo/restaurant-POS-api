3dd4e94fc6f3ab47da0d6728936545255a77c72e
OneZerocococo <ylflyfly@gmail.com>
2023-01-07 14:13:09 +0800

    feat: add method-override

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require('express')
+const methodOverride = require('method-override')
 const app = express()
 
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
+app.use(methodOverride('_method'))
 
 app.get('/', (req, res) => {
   res.send('hello world')
