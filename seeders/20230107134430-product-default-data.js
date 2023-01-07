88b2c627b8c9c79dd192d5c261b1477f83064b7e
OneZerocococo <ylflyfly@gmail.com>
2023-01-07 22:12:32 +0800

    feat: add product seeder

---
+++ seeders/20230107134430-product-default-data.js
@@ -0,0 +1,24 @@
+'use strict'
+const faker = require('faker')
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    const seedProducts = Array.from({ length: 10 }, (_, i) => ({
+      category_id: Math.floor(Math.random() * 3 + 1),
+      name: `餐點${i + 1}`,
+      name_en: `Product${i + 1}`,
+      description: faker.lorem.paragraph().substring(0, 20),
+      image: `https://loremflickr.com/320/240/meal/?lock=${i + 1}`,
+      cost: 150,
+      price: 300,
+      created_at: new Date(),
+      updated_at: new Date()
+    }))
+    await queryInterface.bulkInsert('Products', seedProducts, {})
+  },
+
+  async down (queryInterface, Sequelize) {
+    await queryInterface.bulkDelete('Products', null, {})
+  }
+}
