959863781ec817d0aabc7bb6a693a9c875b5e3f1
OneZerocococo <ylflyfly@gmail.com>
2023-01-10 15:26:51 +0800

    feat: add cors setting

--- app.js
+++ app.js
@@ -5,10 +5,12 @@ if (process.env.NODE_ENV !== 'production') {
 const express = require('express')
 const passport = require('./config/passport')
 const routes = require('./routes')
+const cors = require('cors')
 const methodOverride = require('method-override')
 const app = express()
 const port = process.env.PORT || 3000
 
+app.use(cors({ origin: [`http://localhost:${port}`, process.env.FRONTEND_ORIGIN] }))
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 app.use(methodOverride('_method'))
